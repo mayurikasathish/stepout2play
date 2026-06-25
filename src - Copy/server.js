7109a1a0add4ca4eea799c16fe952cf6db55e456
mayurika <mayurikasathish@gmail.com>
2026-06-25 11:21:29 +0530

    safety save

--- src - Copy/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`StepOut2Play API running on port ${PORT}`);
-  console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
-});
